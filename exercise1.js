c883815dd84c922099bfef470ddec99449c8b5e0
Tyzaya August <2023159774@ub.edu.bz>
2026-02-03 11:27:25 -0600

    Complete PArt 1: Function declarations and expressions

---
+++ exercise1.js
@@ -0,0 +1,19 @@
+//Exercise 1
+
+function calculateArea(length, width) {
+  return length * width;
+}
+
+console.log("Area calculations:");
+console.log(calculateArea(5, 10));
+console.log(calculateArea(7, 3));
+console.log(calculateArea(4.5, 2.5));
+
+//Exercise 2
+const calculatePerimeter = (length, width) => 2 * (length + width);
+
+console.log("Perimeter calculations:");
+console.log(calculatePerimeter(5, 10));
+console.log(calculatePerimeter(7, 3));
+console.log(calculatePerimeter(9, 4));
+
